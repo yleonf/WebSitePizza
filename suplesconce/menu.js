105b41cd4ffbcaeefc554e4805ec984bbdce5dec
andresroa1 <163021791+andresroa1@users.noreply.github.com>
2024-04-27 20:45:36 -0400

    Delete suplesconce directory

--- suplesconce/menu.js
+++
@@ -1,7 +0,0 @@
-let btnMenu=document.getElementById('btnmenu');
-let menu=document.getElementById('menu');
-btnMenu.addEventListener('click',function(){
-    'user strict';
-    menu.classList.toggle('mostrar');
- 
-});
